7b948145dbbb8d5cc08f60803bfc9812c670c27b
Carl LeCompte <43455988+plsmphnx@users.noreply.github.com>
2023-10-19 00:59:20 -0700

    Generalize systems iterator.

--- flake.nix
+++ flake.nix
@@ -1,22 +1,18 @@
 {
   outputs = { nixpkgs, ... }: let
-    attrs = k: f: builtins.listToAttrs
-      (map (n: { name = n; value = f n; }) k);
+    systems = fn: nixpkgs.lib.mapAttrs (_: fn) nixpkgs.legacyPackages;
   in {
-    packages = attrs [ "x86_64-linux" "aarch64-linux" ]
-      (system: let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        default = pkgs.stdenv.mkDerivation {
-          name = "redact-pdf";
-          src = ./.;
-          buildInputs = [ pkgs.qpdf ];
-          buildPhase = "./build";
-          installPhase = ''
-            mkdir -p "$out/bin"
-            mv redact-pdf "$out/bin"
-          '';
-        };
-      });
+    packages = systems (pkgs: {
+      default = pkgs.stdenv.mkDerivation {
+        name = "redact-pdf";
+        src = ./.;
+        buildInputs = [ pkgs.qpdf ];
+        buildPhase = "./build";
+        installPhase = ''
+          mkdir -p "$out/bin"
+          mv redact-pdf "$out/bin"
+        '';
+      };
+    });
   };
 }
